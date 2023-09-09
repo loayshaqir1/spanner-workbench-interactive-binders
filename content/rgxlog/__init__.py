5bb0e095cdd89b0844de310e925fbca1e913c64c
LoayShaqir <loaishaqir@campus.technion.ac.il>
2023-09-09 12:35:57 +0300

    add rgxlog to content folder

---
+++ content/rgxlog/__init__.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+import sys
+sys.path.append('/drive/spanner_workbench/src/rgxlog_interpreter/src/')
+sys.path.append('/drive/spanner_workbench/src/rgxlog_interpreter/src/rgxlog/')
+
+import rgxlog.engine
+import rgxlog.grammar
+from rgxlog.engine.session import Session
+from rgxlog.magic.rgxlog_magic import RgxlogMagic
+from IPython import InteractiveShell, get_ipython
+
+magic_session = Session()
+
+
+def load_ipython_extension(ipython: InteractiveShell) -> None:
+    # this method gets called when running `%load_ext rgxlog` or `import rgxlog` in jupyter
+    ipython.register_magics(RgxlogMagic)
+
+
+try:
+    load_ipython_extension(get_ipython())
+
+except (AttributeError, ImportError):
+    pass
